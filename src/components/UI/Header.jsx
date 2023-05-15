69306f4f4c6c4f1786867308db7e6ef59d1577d9
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-15 16:06:27 +0300

    add header links for user

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,14 +1,32 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import UsersContext from "../../contexts/UsersContext"
 
 const Header = () => {
+  const { currentUser, setCurrentUser } = useContext(UsersContext)
+  const navigate = useNavigate()
+
   return (
     <header>
       <nav>
 
         <img src="https://marketsplash.com/content/images/2022/06/Paul_Frank-logo-883DB9FC0E-seeklogo.com.png" alt="monke logo" />
         <ul>
-          <li><Link to="/login">login</Link></li>
-          <li><Link to="/register"><button>Register</button></Link></li>
+          {
+            !currentUser ?
+              <>
+                <li><Link to="/login">login</Link></li>
+                <li><Link to="/register"><button>Register</button></Link></li>
+              </> :
+              <>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/">Add new post</Link></li>
+                <li><button onClick={() => {
+                  setCurrentUser(null)
+                  navigate('/register')
+                }}>Log Out</button></li>
+              </>
+          }
         </ul>
       </nav>
 
