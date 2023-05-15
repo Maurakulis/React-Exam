cc4db296fa7f42207c77cc35c3004713adffc693
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-15 13:17:25 +0300

    add user data to data.json

---
+++ src/contexts/UsersContext.jsx
@@ -0,0 +1,65 @@
+import { createContext, useEffect, useReducer, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+const UsersContext = createContext()
+
+export const USERS_ACTION_TYPES = {
+  GET: 'get_all_users',
+  REGISTER: 'register',
+  ADD: 'add_user',
+}
+
+const DATA = 'http://localhost:8080/users'
+
+const reducer = (users, action) => {
+  switch (action.type) {
+    case USERS_ACTION_TYPES.GET:
+      return action.data
+    case USERS_ACTION_TYPES.REGISTER:
+
+      const newUser = {
+        email: action.email,
+        password: action.password,
+        passwordConfirm: action.passwordConfirm,
+      }
+
+      fetch(DATA, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+      })
+      return [...users, newUser]
+    case USERS_ACTION_TYPES.ADD:
+      return [...users, action.data]
+    default:
+      return users
+  }
+}
+
+const UsersProvider = ({ children }) => {
+  const [users, dispatch] = useReducer(reducer, [])
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(DATA)
+
+      const data = await res.json()
+      dispatch({
+        type: USERS_ACTION_TYPES.GET,
+        data: data
+      })
+    })()
+  }, [])
+
+  return (
+    <UsersContext.Provider value={{ users, dispatch, currentUser, setCurrentUser }}>
+      {children}
+    </UsersContext.Provider>
+  )
+}
+
+export { UsersProvider }
+export default UsersContext
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { UsersProvider } from './contexts/UsersContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <UsersProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UsersProvider>
+
 )
