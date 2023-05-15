2c7e9c18eae637ff21ea3cd702f3cc9888d389d3
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-15 10:33:02 +0300

    create register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './components/styles/App.css'
+import './styles/App.css'
 import Header from './components/UI/Header'
+import { Route, Routes } from 'react-router-dom'
+import Register from './components/Pages/Register'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='register' element={<Register />} />
+      </Routes>
     </>
   )
 }
