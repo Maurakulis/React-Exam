b558e9b37c6f0fdbe0b5f1bdf2bb1ea5a15abcde
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-15 19:22:26 +0300

    change home page style

--- src/components/UI/Post.jsx
+++ src/components/UI/Post.jsx
@@ -3,7 +3,10 @@ const Post = ({ post }) => {
   return (
     <div className="card">
       <img src={post.imageUrl} alt={post.title} />
-      <p>{post.title}</p>
+      <div>
+        <h5>{post.title}</h5>
+        <p>{post.description}</p>
+      </div>
     </div>
   )
 }
